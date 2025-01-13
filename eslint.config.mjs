79cb68fe847cc8bfeb6d9202239ed6780b0d2f18
Alexander Bakke <bakkealex@outlook.com>
2025-01-13 09:53:14 +0100

    test changes applied to the eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,28 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginJest from 'eslint-plugin-jest';
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { languageOptions: { globals: { ...globals.node, ...globals.jest } } },
   pluginJs.configs.recommended,
+  {
+    plugins: {
+      jest: pluginJest,
+    },
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+      },
+    },
+    rules: {
+      ...pluginJest.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: ['.eslintrc.cjs'],
+  },
 ];
